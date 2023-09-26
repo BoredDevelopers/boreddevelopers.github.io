c376dc7055fcee0ab37080ef303bea92798d78d1
Jörgen Nilsson <jorgennilsson@live.com>
2023-09-26 14:08:09 +0200

    Update MDXComponents.tsx
    removed ComponentMap

--- components/MDXComponents.tsx
+++ components/MDXComponents.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/display-name */
 import React, { useMemo } from 'react'
-import { ComponentMap, getMDXComponent } from 'mdx-bundler/client'
+import { getMDXComponent } from 'mdx-bundler/client'
 import Image from './Image'
 import CustomLink from './Link'
 import TOCInline from './TOCInline'
@@ -12,7 +12,7 @@ const Wrapper: React.ComponentType<{ layout: string }> = ({ layout, ...rest }) =
   return <Layout {...rest} />
 }
 
-export const MDXComponents: ComponentMap = {
+export const MDXComponents = {
   Image,
   //@ts-ignore
   TOCInline,
@@ -32,5 +32,5 @@ interface Props {
 export const MDXLayoutRenderer = ({ layout, mdxSource, ...rest }: Props) => {
   const MDXLayout = useMemo(() => getMDXComponent(mdxSource), [mdxSource])
 
-  return <MDXLayout layout={layout} components={MDXComponents} {...rest} />
+  return <MDXLayout layout={layout} components={MDXComponents as any} {...rest} />
 }
