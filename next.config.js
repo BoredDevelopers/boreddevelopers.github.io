9f3c06479e725d9ae53b59223332ce73a5e422c1
AirBnBacon <115998408+AirBnBacon@users.noreply.github.com>
2023-09-26 14:18:33 +0200

    Update next.config.js
    disabled image optimisation in next.config.js

--- next.config.js
+++ next.config.js
@@ -69,6 +69,9 @@ module.exports = withBundleAnalyzer({
       },
     ]
   },
+  images: {
+    unoptimized: true,
+  },
   webpack: (config, { dev, isServer }) => {
     config.module.rules.push({
       test: /\.(png|jpe?g|gif|mp4)$/i,
